cbac537beb3f10cd5c324081b2e0f704da8676ac
vishnu-hcah <vishnu.gupta@hcah.in>
2022-11-30 21:34:01 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import ReactEcharts from "echarts-for-react";
+
+const wineData = require('./Wine-Data.json')
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [scatterOption, setScatterOption] = useState<any>({
+    xAxis: {},
+    yAxis: {},
+    series: [
+      {
+        data: [
+        ],
+        type: 'scatter'
+      }
+    ]
+  })
+  const [chartOption, setChartOption] = useState<any>({
+    xAxis: {
+      type: 'category',
+      data: []
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [],
+        type: 'bar'
+      }
+    ]
+  });
 
+  useEffect(() => {
+    let colorIntensity: any = [];
+    let alcohol: Number[] = [];
+    let malicAcid: Number[] = [];
+
+    wineData.forEach((element: any) => {
+      colorIntensity.push([element["Color intensity"], element["Hue"]])
+      alcohol.push(element["Alcohol"])
+      malicAcid.push(element["Malic Acid"])
+    });
+
+    setChartOption({
+      ...chartOption,
+      xAxis: {
+        type: 'category',
+        data: alcohol
+      },
+      series: [{ type: 'bar', data: alcohol }]
+    })
+
+    setScatterOption({
+      ...scatterOption, series: [{
+        scatterOption: 4,
+        data: colorIntensity,
+        type: 'scatter'
+      }]
+    })
+
+  }, []);
+
+  return <div style={{
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }}>
+    <div className="w-50">
+      <ReactEcharts option={scatterOption} />
+    </div>
+    <div className="w-50">
+      <ReactEcharts option={chartOption} />
+    </div>
+  </div>;
+}
 export default App;
